d35d3da1d986f8ec6a7072ca40acb1e909e7aa02
jphabs <jpbermoy@gmail.com>
2025-03-05 14:07:55 +0800

    auth.js update v3

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
-<!-- firebase-config.js -->
-<script>
-  // Your Firebase configuration object
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyAYqvRzV-wlaVPU8j1YolZLyH36PCvlT_0",
     authDomain: "tweaks-ph-login-server.firebaseapp.com",
     projectId: "tweaks-ph-login-server",
-    storageBucket: "tweaks-ph-login-server.appspot.com", // Fixed: .appspot.com
+    storageBucket: "tweaks-ph-login-server.appspot.com",  // Fixed value
     messagingSenderId: "970111986272",
     appId: "1:970111986272:web:fa1d7b107608b094a67105"
   };
 
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  // Optionally, assign auth to a global variable:
-  window.auth = firebase.auth();
+  const app = initializeApp(firebaseConfig);
 </script>
