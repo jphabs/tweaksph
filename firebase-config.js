e47f405f1c2ce5c444c41f615d2f7675437e5937
jphabs <jpbermoy@gmail.com>
2025-03-05 13:54:30 +0800

    update firebase v1

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+<!-- firebase-config.js -->
+<script>
+  // Your Firebase configuration object
+  const firebaseConfig = {
+    apiKey: "AIzaSyAYqvRzV-wlaVPU8j1YolZLyH36PCvlT_0",
+    authDomain: "tweaks-ph-login-server.firebaseapp.com",
+    projectId: "tweaks-ph-login-server",
+    storageBucket: "tweaks-ph-login-server.appspot.com", // Fixed: .appspot.com
+    messagingSenderId: "970111986272",
+    appId: "1:970111986272:web:fa1d7b107608b094a67105"
+  };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAYqvRzV-wlaVPU8j1YolZLyH36PCvlT_0",
-  authDomain: "tweaks-ph-login-server.firebaseapp.com",
-  projectId: "tweaks-ph-login-server",
-  storageBucket: "tweaks-ph-login-server.appspot.com",
-  messagingSenderId: "970111986272",
-  appId: "1:970111986272:web:fa1d7b107608b094a67105"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Optionally, assign auth to a global variable:
+  window.auth = firebase.auth();
+</script>
