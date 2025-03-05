f2f3a194ee8b5e62020c40e0de73dad6eea17536
jphabs <jpbermoy@gmail.com>
2025-03-05 14:39:57 +0800

    auth.js update v7

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,14 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAYqvRzV-wlaVPU8j1YolZLyH36PCvlT_0",
-    authDomain: "tweaks-ph-login-server.firebaseapp.com",
-    projectId: "tweaks-ph-login-server",
-    storageBucket: "tweaks-ph-login-server.appspot.com",  // Fixed value
-    messagingSenderId: "970111986272",
-    appId: "1:970111986272:web:fa1d7b107608b094a67105"
-  };
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAYqvRzV-wlaVPU8j1YolZLyH36PCvlT_0",
+  authDomain: "tweaks-ph-login-server.firebaseapp.com",
+  projectId: "tweaks-ph-login-server",
+  storageBucket: "tweaks-ph-login-server.appspot.com",  // Correct format
+  messagingSenderId: "970111986272",
+  appId: "1:970111986272:web:fa1d7b107608b094a67105"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
