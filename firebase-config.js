dcb3f71dbea8e664d1c303360ab0e3d908848cd3
jphabs <jpbermoy@gmail.com>
2025-03-05 13:37:02 +0800

    firebase update

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,14 @@
-// firebase-config.js (Global Version for GitHub Pages)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAYqvRzV-wlaVPU8j1YolZLyH36PCvlT_0",
   authDomain: "tweaks-ph-login-server.firebaseapp.com",
   projectId: "tweaks-ph-login-server",
-  storageBucket: "tweaks-ph-login-server.appspot.com",  // Fixed value
+  storageBucket: "tweaks-ph-login-server.appspot.com",
   messagingSenderId: "970111986272",
   appId: "1:970111986272:web:fa1d7b107608b094a67105"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
